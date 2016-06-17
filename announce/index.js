791bcca413d5499ac48e254e13b9cecf756b5ca7
Jackbennett <jbennett@birkdalehigh.co.uk>
2016-06-17 14:02:27 +0100

    share the req call and implement down()

--- announce/index.js
+++ announce/index.js
@@ -4,112 +4,58 @@ const url = require('url')
 const os = require('os')
 const _ = require('lodash')
 
+var currentProject
+
 function up(from, to) {
-
-  var to = to || `http://${os.hostname()}`
-
-  //  if (http.address) {
-  //    var port = http.address().port
-  //  }
-
+  var port = parseInt(process.env.PORT || 3000)
   _.defaults(from, {
-    port: parseInt(process.env.PORT || 3000),
-    url: guessUrl(),
+    port: port,
+    url: guessUrl(port),
     name: os.hostname()
   })
 
-  _.defaults(to, getAPI())
+  _.defaults(to, {
+    hostname: os.hostname(),
+    port: 3000
+  })
 
-  function guessUrl() {
-    // Create the address of the project to advertise
-    var prefix = from.port === 443 ? 'https' : 'http'
-    return `${prefix}://${os.hostname()}:${from.port}`.replace(/:80$|:443$/, '/')
-  }
+  currentProject = from
 
-  function getAPI() {
-    return url.parse(to || process.env.NODE_ORG_API)
-  }
-
-  var postData = querystring.stringify(from)
-
-  var options = {
-    path: '/announce',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': postData.length
-    }
-  }
-  _.defaults(options, to)
-
-  var req = http.request(options, (res) => {
-    console.log(`STATUS: ${res.statusCode}`);
-    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-    res.setEncoding('utf8');
-
-    res.on('data', (chunk) => {
-      console.log(`BODY: ${chunk}`);
-    });
-
-    res.on('end', () => {
-      console.log('No more data in response.')
-    })
-  });
-
-  req.on('error', (e) => {
-    console.log(`problem with request: ${e.message}`);
-  });
-
-  // write data to request body
-  req.write(postData);
-  req.end();
+  request('post', currentProject)
 }
 
-function down(from, to) {
-  var to = to || `http://${os.hostname()}`
+function down() {
+  request('put', currentProject, true)
+}
 
-  if (http.address) {
-    var port = http.address().port
-  }
-  var defFrom = {
-    port: parseInt(port || process.env.PORT || 3000)
-  }
-  _.defaults(from, defFrom, {
-    url: guessUrl(),
-    name: os.hostname()
-  })
+function guessUrl(data) {
+  // Create the address of the project to advertise
+  var prefix = data === 443 ? 'https' : 'http'
+  return `${prefix}://${os.hostname()}:${data}`.replace(/:80$|:443$/, '/')
+}
 
-  _.defaults(to, getAPI())
-
-  function guessUrl() {
-    // Create the address of the project to advertise
-    var prefix = defFrom.port === 443 ? 'https' : 'http'
-    return `${prefix}://${os.hostname()}:${defFrom.port}`.replace(/:80$|:443$/, '/')
-  }
-
-  function getAPI() {
-    return url.parse(to || process.env.NODE_ORG_API)
-  }
-
-  var postData = querystring.stringify(from)
+function request(method, data, exit) {
+  console.log(`HTTP: ${method} about ${data.url}`)
+  var postData = querystring.stringify(data)
 
   var options = {
+    hostname: 'ITSPC02',
+    port: 3000,
     path: '/announce',
-    method: 'PUT',
+    method: method,
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
       'Content-Length': postData.length
     }
   }
-  _.defaults(options, to)
 
   var req = http.request(options, (res) => {
-    console.log(`STATUS: ${res.statusCode}`);
-    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-    res.setEncoding('utf8');
+    console.log(`STATUS: ${res.statusCode}`)
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`)
+    res.setEncoding('utf8')
 
     res.on('data', (chunk) => {
-      console.log(`BODY: ${chunk}`);
+      console.log(`BODY: ${chunk}`)
     });
 
     res.on('end', () => {
@@ -118,22 +64,21 @@ function down(from, to) {
   });
 
   req.on('error', (e) => {
-    console.log(`problem with request: ${e.message}`);
+    console.log(`problem with request: ${e.message}`)
   });
-
-  // write data to request body
+  req.setTimeout(500)
+    // write data to request body
   req.write(postData);
-  req.end();
+  req.end(() => {
+    if (exit) process.exit()
+  });
 }
 
 process.stdin.resume(); //so the program will not close instantly
 
 function exitHandler(options, err) {
-  console.log('PUT close message for URL')
+  console.log('announce: PUT for URL')
   down()
-  if (options.cleanup) console.log('clean');
-  if (err) console.log(err.stack);
-  if (options.exit) process.exit();
 }
 
 //do something when app is closing
